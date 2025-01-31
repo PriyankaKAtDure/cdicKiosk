69a3067eab77b8ec8ca049fa146cf371378420ba
PriyankaKAtDure <102956542+PriyankaKAtDure@users.noreply.github.com>
2025-01-31 17:57:16 +0530

    line list ui fixes

--- src/Pages/Patient.js
+++ src/Pages/Patient.js
@@ -159,7 +159,7 @@ export default function Patient() {
               }} className="" />
           </div>
         </div>
-        <video autoplay muted loop id="myVideo">
+        <video muted loop id="myVideo" autoPlay>
           <source src={videoFile} type="video/mp4" />
         </video>
         <Dialog
