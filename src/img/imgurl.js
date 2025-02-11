dc710d5c0db6f13517f4c736701f0ae601a4e354
PriyankaKAtDure <102956542+PriyankaKAtDure@users.noreply.github.com>
2025-02-11 18:17:30 +0530

    generate icon ui fix

--- src/img/imgurl.js
+++ src/img/imgurl.js
@@ -5,7 +5,7 @@ const imgUrl  ={
   'patientnews': require('../img/new-patient.png'),
   'existingpatient': require('../img/existing-patient.png'),
   'qrCode' : require('../img/qr-code.png'),
-  'generateReport': require('../img/generatereport.png'),
+  'generateReport': require('../img/generatereport.jpg'),
   'patientSummary': require('../img/patient-summary.png'),
   'voiceAudioIcon': require('../img/voiceIcon.gif'),
   'startButtonIcon' : require('../img/startButton.png'),
